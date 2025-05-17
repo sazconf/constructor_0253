98188e05b75cecc57508608b3d6d6fd493ed40c9
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-17 15:34:12 +0700

    Implementasi konstruktor

--- constructor.cpp
+++ constructor.cpp
@@ -14,3 +14,22 @@ public:
     mahasiswa(int iNim, string iNama);   // Constructor dengan 2 parameter
     void cetak();                        // Fungsi cetak
 };
+
+// Implementasi konstruktor
+mahasiswa::mahasiswa() {
+    nim = 0;
+    nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+    nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+    nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
