d21b32d13f9c81e250380f62894af80e21457e71
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-17 15:41:09 +0700

    prosedure printALL

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -24,3 +24,9 @@ int mahasiswa::nim = 0;
 void mahasiswa::setID() {
     id = ++nim;
 }
+
+void mahasiswa::printAll() {
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
