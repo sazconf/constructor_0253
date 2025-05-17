f461f550eaccd9cc049bec49f2b454d413463c67
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-17 15:40:00 +0700

    buat class mahasiswa

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }   // Definisi function
+    static int getNim() { return nim; }           // Definisi function
+
+    mahasiswa(string pnama) : nama(pnama) { setID(); }
+};
