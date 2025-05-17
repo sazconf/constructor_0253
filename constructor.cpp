ec04e7dfe213cb6d8142c4d35d2dea59fbe3c1e2
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-17 15:33:39 +0700

    add class mahasiswa

--- constructor.cpp
+++ constructor.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+private:
+    int nim;
+    string nama;
+
+public:
+    mahasiswa();                         // Default constructor
+    mahasiswa(int);                      // Constructor dengan parameter int
+    mahasiswa(string);                   // Constructor dengan parameter string
+    mahasiswa(int iNim, string iNama);   // Constructor dengan 2 parameter
+    void cetak();                        // Fungsi cetak
+};
