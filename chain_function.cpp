51f646356a0759ca76e5463ee99d9060c23b35bf
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-17 15:05:24 +0700

    Write class buku

--- chain_function.cpp
+++ chain_function.cpp
@@ -15,9 +15,4 @@ public:
     }
 } bukunya;
 
-int main() {
-    // bukunya.setJudul("Matematika");
-    // cout << bukunya.getJudul();
-    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
-    return 0;
-}
+
