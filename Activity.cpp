a6c7aff74551dacb0694e5bf94bd3b6ec97e30ec
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-19 18:21:30 +0700

    Membuat class mahasiswa

--- Activity.cpp
+++ Activity.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//membuat class mahasiswa
+
+class Mahasiswa {
+    private:
+        static int studentTotal;
+    public:
+        string name;
+        string status;
+        int age;
+
+};
+
+
