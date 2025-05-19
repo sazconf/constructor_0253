1172a57da20c53fd258db5f74e6b9d07b476ef9e
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-19 19:04:24 +0700

    Final edit

--- Activity.cpp
+++ Activity.cpp
@@ -30,6 +30,10 @@ class Student {
             return studentTotal;
         }
 
+        static void setStudentTotal(int a){
+            studentTotal = a;
+        }
+
 };
 
 int Student::studentTotal = 0;
@@ -42,17 +46,19 @@ int main (){
     Student st1("Maman", 40);
     Student st2("Agus", 34);
 
+    Student::setStudentTotal(10);
+
     cout<<"Student Total is "<< 
     Student::getStudentTotal()<<endl;
     
 
-    {
+    
         Student st3("Budi", 19);
         Student st4("Caca", 21);
 
         cout<<"Student Total is "<< 
         Student::getStudentTotal()<<endl;
-    }
+    
 
     cout<<"Student Total is "<< 
     Student::getStudentTotal()<<endl;
